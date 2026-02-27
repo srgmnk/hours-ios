9560ae8873e9c6d91d8e33e50eb68daee215b93f
Serg Minkin <minkin.sergey@gmail.com>
2026-02-27 18:04:37 +0700

    Initial Commit

---
+++ Timie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Timie
+//
+//  Created by Sergy on 27.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Timie/TimieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimieApp.swift
+//  Timie
+//
+//  Created by Sergy on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct TimieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
